e888b8790ff2efa0d43a43d6cbf43753f6c988ac
filipepre <49457814+filipepre@users.noreply.github.com>
2021-12-28 22:55:53 +0000

    Ex4

---
+++ Exercicio4/src/main/java/com/mycompany/exercicio4/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercicio4;
+
+/**
+ *
+ * @author Pc
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
